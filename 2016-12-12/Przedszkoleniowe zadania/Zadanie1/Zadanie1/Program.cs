cc9b2815c7ba6c5f74e3f4992a9e88f96fc7c309
Rafał Markowicz <rafal.markowicz@codesprinters.com>
2016-11-28 09:32:19 +0100

    Zadania dla uczestników na automatyzację 2016-12-12

---
+++ 2016-12-12/Przedszkoleniowe zadania/Zadanie1/Zadanie1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = new SayHello();
+            hello.Say();
+
+            Bravo();
+        }
+
+        private static void Bravo()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Brawo! pierwsze zadanie zrobione :)");
+            Console.WriteLine("");
+            Console.WriteLine("naciśnij jakikolwiek klawisz aby zakończyć");
+            Console.ReadKey();
+        }
+    }
+
+    private class SayHello
+    {
+        public void Say()
+        {
+            Console.WriteLine("Hello world");
+          
+        }
+    }
+}
