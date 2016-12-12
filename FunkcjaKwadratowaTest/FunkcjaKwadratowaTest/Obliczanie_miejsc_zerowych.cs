c0ad56af689c5a21d03a8a97303ae40cc7141b90
piotr-w <piotrw@tema.com.pl>
2016-12-12 16:52:55 +0100

    funkcja kwadratowa

---
+++ FunkcjaKwadratowaTest/FunkcjaKwadratowaTest/Obliczanie_miejsc_zerowych.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FunkcjaKwadratowaTest
+{
+    public class Obliczanie_miejsc_zerowych
+    {
+        [Fact]
+        public void First_test()
+        {
+            var a = 1;
+            var b = -4;
+            var c = 3;
+
+            var wynik = Oblicz(a, b, c);
+            Assert.Equal(1, wynik.MiejscePierwsze);
+            Assert.Equal(3, wynik.MiejsceDrugie);
+        }
+
+        private Wynik Oblicz(int a, int b, int c)
+        {
+            Wynik w = new Wynik();
+            w.Oblicz(a, b, c);
+            return w;
+        }
+    }
+}
---
+++ FunkcjaKwadratowaTest/FunkcjaKwadratowaTest/Wynik.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunkcjaKwadratowaTest
+{
+    class Wynik
+    {
+        public double MiejsceDrugie { get; internal set; }
+        public double MiejscePierwsze { get; internal set; }
+
+        public void Oblicz(double a, double b, double c)
+        {
+            double delta = b * b - 4 * a * c;
+            if (delta > 0)
+            {
+                var deltaSqrt = Math.Sqrt(delta);
+                MiejscePierwsze = (-b - deltaSqrt) / (2 * a);
+                MiejsceDrugie = (-b + deltaSqrt) / (2 * a);
+            }
+            else if (delta == 0)
+            {
+                MiejscePierwsze = MiejsceDrugie = -b / (2 * a);
+
+            }
+            else
+            {
+                MiejscePierwsze = double.NaN;
+                MiejsceDrugie = double.NaN;
+            }
+        }
+    }
+}
