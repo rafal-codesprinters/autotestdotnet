c71717d515c04f1a67ee93cbac0339bfefeef92b
Damian Bednarczyk <damian.bednarczyk@kotrak.pl>
2016-12-12 13:09:31 +0100

    Rozwiazanie asercji

--- Koans_2/CSharp/AboutAsserts.cs
+++ Koans_2/CSharp/AboutAsserts.cs
@@ -8,30 +8,30 @@ namespace DotNetKoans.CSharp
         [Koan(1)]
         public void AssertTruth() 
         {
-			Assert.True(false); //This should be true
+			Assert.True(true); //This should be true
         }
 
         //Enlightenment may be more easily achieved with appropriate messages
         [Koan(2)]
         public void AssertTruthWithMessage() 
         {
-            Assert.True(false, "This should be true -- Please fix this");
+            Assert.True(true, "This should be true -- Please fix this");
         }
 
         //To understand reality, we must compare our expectations against reality
         [Koan(3)]
         public void AssertEquality() 
         {
-            var expectedValue = 3;
+            var expectedValue = 2;
             var actualValue = 1 + 1;
             Assert.True(expectedValue == actualValue);
         }
 
-        //Some wasy of asserting equality are better than others
+        //Some ways of asserting equality are better than others
         [Koan(4)]
         public void ABetterWayOfAssertingEquality() 
         {
-            var expectedValue = 3;
+            var expectedValue = 2;
             var actualValue = 1 + 1;
             Assert.Equal(expectedValue, actualValue);
         }
@@ -40,6 +40,7 @@ namespace DotNetKoans.CSharp
         [Koan(5)]
         public void FillInValues() 
         {
+            var FILL_ME_IN = 2;
             Assert.Equal(FILL_ME_IN, 1 + 1);
         }
     }
