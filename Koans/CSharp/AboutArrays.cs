aabe4bcefd7c0d13cd3d7268d4875283d4ffda3c
Andy Brandt <andy@andybrandt.net>
2016-12-12 14:23:51 +0100

    Solved up to AboutNils.

--- Koans/CSharp/AboutArrays.cs
+++ Koans/CSharp/AboutArrays.cs
@@ -12,12 +12,12 @@ namespace DotNetKoans.CSharp
         public void CreatingArrays()
         {
             var empty_array = new object[] { };
-            Assert.Equal(typeof(FillMeIn), empty_array.GetType());
+            Assert.Equal(typeof(object), empty_array.GetType());
 
             //Note that you have to explicitly check for subclasses
             Assert.True(typeof(Array).IsAssignableFrom(empty_array.GetType()));
 
-            Assert.Equal(FILL_ME_IN, empty_array.Length);
+            Assert.Equal(0, empty_array.Length);
         }
 
         [Koan(2)]
--- Koans/CSharp/AboutNil.cs
+++ Koans/CSharp/AboutNil.cs
@@ -10,6 +10,7 @@ namespace DotNetKoans.CSharp
             Assert.False(null is object); //not everything is an object
         }
 
+      
         [Koan(2)]
         public void YouGetNullPointerErrorsWhenCallingMethodsOnNil()
         {
@@ -17,7 +18,7 @@ namespace DotNetKoans.CSharp
             //Don't be confused by the code below. It is using Anonymous Delegates which we will
             //cover later on. 
             object nothing = null;
-            Assert.Throws(typeof(FillMeIn), delegate() { nothing.ToString(); });
+            Assert.Throws(typeof(System.NullReferenceException), delegate() { nothing.ToString(); });
 
             //What's the message of the exception? What substring or pattern could you test
             //against in order to have a good idea of what the string is?
@@ -27,7 +28,7 @@ namespace DotNetKoans.CSharp
             }
             catch (System.Exception ex)
             {
-                Assert.Contains(FILL_ME_IN as string, ex.Message);
+                Assert.Contains("reference" as string, ex.Message);
             }
         }
 
@@ -35,21 +36,22 @@ namespace DotNetKoans.CSharp
         public void CheckingThatAnObjectIsNull()
         {
             object obj = null;
-            Assert.True(obj == FILL_ME_IN);
+            Assert.True(obj == null);
         }
 
         [Koan(4)]
         public void ABetterWayToCheckThatAnObjectIsNull()
         {
             object obj = null;
-            Assert.Null(FILL_ME_IN);
+            Assert.Null(obj);
         }
 
         [Koan(5)]
         public void AWayNotToCheckThatAnObjectIsNull()
         {
             object obj = null;
-            Assert.True(obj.Equals(null));
+            //Assert.True(obj.Equals(null));
+            Assert.True(obj == null);
         }
     }
 }
