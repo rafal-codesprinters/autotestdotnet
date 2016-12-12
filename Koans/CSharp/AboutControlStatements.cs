90879bd2166e64c9181b4894a4f6c8136c856aef
Michał Kornacki <m.kornacki@benefitsystems.pl>
2016-12-12 15:07:22 +0100

    part two

--- Koans/CSharp/AboutControlStatements.cs
+++ Koans/CSharp/AboutControlStatements.cs
@@ -19,7 +19,7 @@ namespace DotNetKoans.CSharp
                 b = false;
             }
 
-            Assert.Equal(FILL_ME_IN, b);
+            Assert.Equal(true, b);
         }
 
         [Koan(2)]
@@ -31,7 +31,7 @@ namespace DotNetKoans.CSharp
             else
                 b = false;
 
-            Assert.Equal(FILL_ME_IN, b);
+            Assert.Equal(true, b);
 
         }
 
@@ -44,7 +44,7 @@ namespace DotNetKoans.CSharp
                 b = true;
             }
 
-            Assert.Equal(FILL_ME_IN, b);
+            Assert.Equal(true, b);
         }
 
         [Koan(4)]
@@ -54,7 +54,7 @@ namespace DotNetKoans.CSharp
             if (true)
                 b = true;
 
-            Assert.Equal(FILL_ME_IN, b);
+            Assert.Equal(true, b);
         }
 
         [Koan(5)]
@@ -69,15 +69,15 @@ namespace DotNetKoans.CSharp
                 b1 = true;
                 b2 = true;
 
-			Assert.Equal(FILL_ME_IN, b1);
-			Assert.Equal(FILL_ME_IN, b2);
+			Assert.Equal(false, b1);
+			Assert.Equal(true, b2);
         }
 
         [Koan(6)]
         public void TernaryOperators()
         {
-            Assert.Equal(FILL_ME_IN, (true ? 1 : 0));
-            Assert.Equal(FILL_ME_IN, (false ? 1 : 0));
+            Assert.Equal(1, (true ? 1 : 0));
+            Assert.Equal(0, (false ? 1 : 0));
         }
 
         //This is out of place for control statements, but necessary for Koan 8
@@ -175,7 +175,7 @@ namespace DotNetKoans.CSharp
             {
                 list[i] = (list[i].ToUpper());
             }
-            Assert.Equal(FILL_ME_IN, list);
+            Assert.Equal(new List<string> { "FISH", "AND", "CHIPS" }, list);
         }
 
         [Koan(14)]
