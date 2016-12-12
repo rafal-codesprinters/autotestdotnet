dab50997eb4f9400f6dc3d2a1989d17ad45e6c68
Szymon Szewczyk <mjnuji@wp.pl>
2016-12-12 15:01:31 +0100

    gupie

--- Koans/CSharp/AboutArrays.cs
+++ Koans/CSharp/AboutArrays.cs
@@ -12,12 +12,12 @@ namespace DotNetKoans.CSharp
         public void CreatingArrays()
         {
             var empty_array = new object[] { };
-            Assert.Equal(typeof(FillMeIn), empty_array.GetType());
+            Assert.Equal(typeof(object[]), empty_array.GetType());
 
             //Note that you have to explicitly check for subclasses
             Assert.True(typeof(Array).IsAssignableFrom(empty_array.GetType()));
 
-            Assert.Equal(FILL_ME_IN, empty_array.Length);
+            Assert.Equal(0, empty_array.Length);
         }
 
         [Koan(2)]
@@ -29,13 +29,13 @@ namespace DotNetKoans.CSharp
             Assert.Equal(new int[] { 42 }, array);
 
             //Are arrays 0-based or 1-based?
-            Assert.Equal(42, array[((int)FILL_ME_IN)]);
+            Assert.Equal(42, array[((int)0)]);
 
             //This is important because...
             Assert.True(array.IsFixedSize);
 
             //...it means we can't do this: array[1] = 13;
-            Assert.Throws(typeof(FillMeIn), delegate() { array[1] = 13; });
+            Assert.Throws(typeof(System.IndexOutOfRangeException), delegate() { array[1] = 13; });
 
             //This is because the array is fixed at length 1. You could write a function
             //which created a new array bigger than the last, copied the elements over, and
@@ -45,7 +45,7 @@ namespace DotNetKoans.CSharp
             Assert.Equal(array, dynamicArray.ToArray());
 
             dynamicArray.Add(13);
-            Assert.Equal((new int[] { 42, (int)FILL_ME_IN}), dynamicArray.ToArray());
+            Assert.Equal((new int[] { 42, (int)13}), dynamicArray.ToArray());
         }
 
         [Koan(3)]
--- Koans/CSharp/AboutNil.cs
+++ Koans/CSharp/AboutNil.cs
@@ -7,18 +7,18 @@ namespace DotNetKoans.CSharp
         [Koan(1)]
         public void NilIsNotAnObject()
         {
-            //Assert.True(typeof(object).IsAssignableFrom(new object().GetType())); //not everything is an object
-            Assert.True(typeof(void).IsAssignableFrom(null)); //not everything is an object
+            //Assert.True(typeof(object).IsAssignableFrom(new object().GetType())); //not everything is an object.
+            Assert.True(typeof(object).IsAssignableFrom(typeof(System.Array))); //not everything is an object
         }
 
-        [Koan(2)]
+        [Koan(2)]  // 7
         public void YouGetNullPointerErrorsWhenCallingMethodsOnNil()
         {
             //What is the Exception that is thrown when you call a method on a null object?
             //Don't be confused by the code below. It is using Anonymous Delegates which we will
             //cover later on. 
             object nothing = null;
-            Assert.Throws(typeof(FillMeIn), delegate() { nothing.ToString(); });
+            Assert.Throws(typeof(System.NullReferenceException), delegate() { nothing.ToString(); });
 
             //What's the message of the exception? What substring or pattern could you test
             //against in order to have a good idea of what the string is?
@@ -28,7 +28,7 @@ namespace DotNetKoans.CSharp
             }
             catch (System.Exception ex)
             {
-                Assert.Contains(FILL_ME_IN as string, ex.Message);
+                Assert.Contains("Object reference not set to an instance of an object" as string, ex.Message);
             }
         }
 
@@ -36,21 +36,21 @@ namespace DotNetKoans.CSharp
         public void CheckingThatAnObjectIsNull()
         {
             object obj = null;
-            Assert.True(obj == FILL_ME_IN);
+            Assert.True(obj == null);
         }
 
         [Koan(4)]
         public void ABetterWayToCheckThatAnObjectIsNull()
         {
             object obj = null;
-            Assert.Null(FILL_ME_IN);
+            Assert.Null(obj);
         }
 
         [Koan(5)]
-        public void AWayNotToCheckThatAnObjectIsNull()
+        public void AWayNotToCheckThatAnObjectIsNull() // teraz jest ok
         {
             object obj = null;
-            Assert.True(obj.Equals(null));
+            Assert.True(obj == null);
         }
     }
 }
