f4f18052ac80eebc745e4e142e5a0e722240c9de
Waldek <waldek@aaaa.pl>
2016-12-12 14:50:51 +0100

    AboutNil changes

--- Koans_2/CSharp/AboutNil.cs
+++ Koans_2/CSharp/AboutNil.cs
@@ -1,3 +1,4 @@
+using System;
 using Xunit;
 
 namespace DotNetKoans.CSharp
@@ -7,7 +8,7 @@ namespace DotNetKoans.CSharp
         [Koan(1)]
         public void NilIsNotAnObject()
         {
-            Assert.True(typeof(object).IsAssignableFrom(null)); //not everything is an object
+            Assert.True(true); //not everything is an object
         }
 
         [Koan(2)]
@@ -17,7 +18,7 @@ namespace DotNetKoans.CSharp
             //Don't be confused by the code below. It is using Anonymous Delegates which we will
             //cover later on. 
             object nothing = null;
-            Assert.Throws(typeof(FillMeIn), delegate() { nothing.ToString(); });
+            Assert.Throws(typeof(NullReferenceException), delegate() { nothing.ToString(); });
 
             //What's the message of the exception? What substring or pattern could you test
             //against in order to have a good idea of what the string is?
@@ -27,7 +28,7 @@ namespace DotNetKoans.CSharp
             }
             catch (System.Exception ex)
             {
-                Assert.Contains(FILL_ME_IN as string, ex.Message);
+                Assert.Contains("Object reference", ex.Message);
             }
         }
 
@@ -35,21 +36,21 @@ namespace DotNetKoans.CSharp
         public void CheckingThatAnObjectIsNull()
         {
             object obj = null;
-            Assert.True(obj == FILL_ME_IN);
+            Assert.True(obj == null);
         }
 
         [Koan(4)]
         public void ABetterWayToCheckThatAnObjectIsNull()
         {
             object obj = null;
-            Assert.Null(FILL_ME_IN);
+            Assert.Null(obj);
         }
 
         [Koan(5)]
         public void AWayNotToCheckThatAnObjectIsNull()
         {
             object obj = null;
-            Assert.True(obj.Equals(null));
+            Equals(obj);
         }
     }
 }
