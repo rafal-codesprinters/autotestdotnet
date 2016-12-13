fb406861e06aa0d4724d92fc08e379f1cbba17fb
unknown <piotrw@tema.com.pl>
2016-12-13 09:53:45 +0100

    funkcja kwadratowa d2 1

--- FunkcjaKwadratowaTest/FunkcjaKwadratowaTest/Obliczanie_miejsc_zerowych.cs
+++ FunkcjaKwadratowaTest/FunkcjaKwadratowaTest/Obliczanie_miejsc_zerowych.cs
@@ -10,26 +10,41 @@ namespace FunkcjaKwadratowaTest
     public class Obliczanie_miejsc_zerowych
     {
         [Fact]
-        public void First_test()
+        public void Weryfikacja_ze_potrafimy_znalezc_dwa_miejsca_zerowe()
         {
             var a = 1;
             var b = -4;
             var c = 3;
 
-            var wynik = Oblicz(a, b, c);
-            Assert.Equal(1, wynik.MiejscePierwsze);
-            Assert.Equal(3, wynik.MiejsceDrugie);
+            var wynik = Oblicz(a, b, c).ToList();
+            Assert.Equal(1, wynik[0]); //wynik.MiejscePierwsze
+            Assert.Equal(3, wynik[1]); //wynik.MiejsceDrugie
         }
 
-        private Wynik Oblicz(int a, int b, int c)
+        [Fact]
+        public void Weryfikacja_ze_potrafimy_znalezc_jedno_miejsce_zerowe()
         {
-            //Wynik w = new Wynik();
-            //w.Oblicz(a, b, c);
-            //return w;
+            var a = 9;
+            var b = -12;
+            var c = 4;
+
+            var wynik = Oblicz(a, b, c);
+            Assert.Equal(2.0/3, wynik.First());
+        }
+
+        private Wynik Oblicz_v1(int a, int b, int c)
+        {
             double delta = b * b - 4 * a * c;
             var x1 = (-b - Math.Sqrt(delta)) / (2 * a);
             var x2 = (-b + Math.Sqrt(delta)) / (2 * a);
             return new Wynik { MiejscePierwsze = x1, MiejsceDrugie = x2 };
+        }
+
+        private IEnumerable<double> Oblicz(double a, double b, double c)
+        {
+            double delta = b * b - 4 * a * c;
+            yield return (-b - Math.Sqrt(delta)) / (2 * a);
+            yield return (-b + Math.Sqrt(delta)) / (2 * a);
         }
     }
 }
