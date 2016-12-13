c23d782a6716a9b79d653dd113cde69b43458f21
Administrator <piotr.malecki@tema.com.pl>
2016-12-13 14:37:16 +0100

    Sprawdzenie działania testu

--- Selenium/test_2 - dodanie postu.cs
+++ Selenium/test_2 - dodanie postu.cs
@@ -1,47 +1,61 @@
 using System;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
-using Xunit;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumTests
 {
-    public class Selenium : IDisposable
+    [TestFixture]
+    public class Test2DodaniePostu
     {
         private IWebDriver driver;
         private StringBuilder verificationErrors;
         private string baseURL;
         private bool acceptNextAlert = true;
-
-        public Selenium()
+        
+        [SetUp]
+        public void SetupTest()
         {
             driver = new FirefoxDriver();
-            driver.Manage().Window.Maximize();
             baseURL = "https://autotestdotnet.wordpress.com/wp-admin/";
             verificationErrors = new StringBuilder();
         }
         
-        [Fact]
-        public void The2DodaniePostuV2CsTest()
+        [TearDown]
+        public void TeardownTest()
         {
-            driver.Navigate().GoToUrl(baseURL + "");
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+        
+        [Test]
+        public void The2DodaniePostuTest()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/wp-login.php?redirect_to=https%3A%2F%2Fautotestdotnet.wordpress.com%2Fwp-admin%2F&reauth=1");
             driver.FindElement(By.Id("user_login")).Clear();
             driver.FindElement(By.Id("user_login")).SendKeys("autotestdotnet@gmail.com");
             driver.FindElement(By.Id("user_pass")).Clear();
             driver.FindElement(By.Id("user_pass")).SendKeys("codesprinters2016");
-            driver.FindElement(By.Id("rememberme")).Click();
             driver.FindElement(By.Id("wp-submit")).Click();
             driver.FindElement(By.XPath("//li[@id='menu-posts']/a/div[3]")).Click();
             driver.FindElement(By.LinkText("Add New")).Click();
             driver.FindElement(By.Id("title")).Clear();
-            driver.FindElement(By.Id("title")).SendKeys("Pan Tadeusz v7");
-            driver.FindElement(By.Id("content")).Clear();
-            driver.FindElement(By.Id("content")).SendKeys("Litwo Ty moja Królowo...");
+            driver.FindElement(By.Id("title")).SendKeys("Pan Tadeusz v4");
             driver.FindElement(By.Id("publish")).Click();
-            driver.FindElement(By.CssSelector("img.avatar.avatar-32")).Click();
-            driver.FindElement(By.CssSelector("button.ab-sign-out")).Click();
-            driver.Navigate().GoToUrl("https://autotestdotnet.wordpress.com");
-            Assert.Equal("Pan Tadeusz v7", driver.FindElement(By.LinkText("Pan Tadeusz v7")).Text);
+            driver.FindElement(By.LinkText("My Site")).Click();
+            driver.FindElement(By.CssSelector("span.ab-site-title")).Click();
+            Assert.AreEqual("Pan Tadeusz v4", driver.FindElement(By.LinkText("Pan Tadeusz v4")).Text);
         }
         private bool IsElementPresent(By by)
         {
@@ -82,19 +96,6 @@ namespace SeleniumTests
             } finally {
                 acceptNextAlert = true;
             }
-        }
-
-        public void Dispose()
-        {
-            try
-            {
-                driver.Quit();
-            }
-            catch (Exception)
-            {
-                // Ignore errors if unable to close the browser
-            }
-            Assert.Equal("", verificationErrors.ToString());
         }
     }
 }
