ea20217f33c009770d99929c3ff8e9901d3180f9
Monika Pawela <monika.pawela@kotrak.com>
2016-12-13 16:50:01 +0100

    IDE

---
+++ Seleniuym_automat_visual/Automat/Automat/WebDriverWait.cs
@@ -0,0 +1,6 @@
+﻿namespace SeleniumTests
+{
+    internal class WebDriverWait
+    {
+    }
+}
--- Seleniuym_automat_visual/Automat/Automat/usuniecieNotki.cs
+++ Seleniuym_automat_visual/Automat/Automat/usuniecieNotki.cs
@@ -2,59 +2,60 @@ using System;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
-using Xunit;
-using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumTests
 {
-
-    public class NotatkaSelIDE : IDisposable
+    [TestFixture]
+    public class UsuniecieNotki
     {
         private IWebDriver driver;
         private StringBuilder verificationErrors;
         private string baseURL;
         private bool acceptNextAlert = true;
-
-        public NotatkaSelIDE()
+        
+        [SetUp]
+        public void SetupTest()
         {
-
-            driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
+            driver = new FirefoxDriver();
             baseURL = "https://autotestdotnet.wordpress.com/";
             verificationErrors = new StringBuilder();
-            driver.Manage()
-                .Timeouts()
-                .ImplicitlyWait(TimeSpan.FromSeconds(10));
         }
         
-        [Fact]
-        public void TheNotatkaSelIDETest()
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+        
+        [Test]
+        public void TheUsuniecieNotkiTest()
         {
             driver.Navigate().GoToUrl(baseURL + "/wp-login.php?redirect_to=https%3A%2F%2Fautotestdotnet.wordpress.com%2Fwp-admin%2F&reauth=1");
             driver.FindElement(By.Id("user_login")).Clear();
             driver.FindElement(By.Id("user_login")).SendKeys("autotestdotnet@gmail.com");
-
-            //waitForElementClickable(By.Id("user_login"), 10);
-
             driver.FindElement(By.Id("user_pass")).Clear();
             driver.FindElement(By.Id("user_pass")).SendKeys("codesprinters2016");
             driver.FindElement(By.Id("wp-submit")).Click();
-            Assert.Equal("Dashboard ‹ Site Title — WordPress", driver.Title);
-            driver.FindElement(By.XPath("//li[@id='menu-posts']/a/div[3]")).Click();
-            Assert.Equal("Posts ‹ Site Title — WordPress", driver.Title);
-            driver.FindElement(By.CssSelector("a.page-title-action")).Click();
-            driver.FindElement(By.Id("title-prompt-text")).Click();
-            driver.FindElement(By.Id("title")).Clear();
-            driver.FindElement(By.Id("title")).SendKeys("Test_MP");
-            driver.FindElement(By.Id("content")).Clear();
-            driver.FindElement(By.Id("content")).SendKeys("tekst....test");
-            driver.FindElement(By.Id("save-post")).Click();
-            Assert.Equal("Edit Post ‹ Site Title — WordPress", driver.Title);
-            driver.FindElement(By.Id("publish")).Click();
-            Assert.Equal("Update", driver.FindElement(By.Id("publish")).GetAttribute("value"));
-            Assert.Equal("Edit Post ‹ Site Title — WordPress", driver.Title);
+            Assert.AreEqual("Dashboard ‹ Site Title — WordPress", driver.Title);
+            driver.FindElement(By.LinkText("All Posts")).Click();
+            driver.FindElement(By.Id("post-search-input")).Clear();
+            driver.FindElement(By.Id("post-search-input")).SendKeys("Test_MP");
+            driver.FindElement(By.Id("search-submit")).Click();
+            Assert.AreEqual("Posts ‹ Site Title — WordPress", driver.Title);
+            driver.FindElement(By.LinkText("Trash")).Click();
+            Assert.AreEqual("Posts ‹ Site Title — WordPress", driver.Title);
             driver.FindElement(By.CssSelector("img.avatar.avatar-32")).Click();
             driver.FindElement(By.CssSelector("button.ab-sign-out")).Click();
         }
@@ -97,21 +98,6 @@ namespace SeleniumTests
             } finally {
                 acceptNextAlert = true;
             }
-        }
-
-        public void Dispose()
-        {
-            try
-            {
-                driver.Quit();
-            }
-            catch (Exception)
-            {
-                // Ignore errors if unable to close the browser
-            }
-            Assert.Equal("", verificationErrors.ToString());
-            //throw new NotImplementedException();
-
         }
     }
 }
