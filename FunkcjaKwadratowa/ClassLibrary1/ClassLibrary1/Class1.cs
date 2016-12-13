e70c1609c5e59515eaaa2f1c2e1d16ae1c99c0ad
Szymon Szewczyk <mjnuji@wp.pl>
2016-12-13 09:42:29 +0100

    Wywalenie testy ktory sprawdza trzy przypadki na rzecz trzech testow ktore sprawdzaja to osobno i wywalaja nie miejsce zerowe tylko ilosc miejsc zerowych.

--- FunkcjaKwadratowa/ClassLibrary1/ClassLibrary1/Class1.cs
+++ FunkcjaKwadratowa/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -16,45 +16,33 @@ namespace ClassLibrary1
             var b = -4;
             var c = 3;
 
-            var wynik = Oblicz(a, b, c);
+            var wynik = Oblicz(a, b, c).ToList();
 
-            Assert.Equal(3, wynik.MiejscePierwsze);
-            Assert.Equal(1, wynik.MiejsceDrugie);
+            Assert.Equal(1, wynik[1]);
+            Assert.Equal(3, wynik[0]);
 
         }
 
-        private Wynik Oblicz(int a, int b, int c)
+        [Fact]
+        public void Weryfikacja_ze_potrafimy_znalezc_jedno_miejsce_zerowe()
         {
-            double MiejscePierwsze = 0;
-            double MiejsceDrugie = 0;
+            var a = 1;
+            var b = -4;
+            var c = 3;
 
+            var wynik = Oblicz(a, b, c).ToList();
+
+            Assert.Equal(1, wynik[1]);
+            Assert.Equal(3, wynik[0]);
+
+        }
+
+        private IEnumerable <double> Oblicz(double a, double b, double c)  //inumerable, nie tworzymy tablicy, inumerable ma numerator, nie określone jak kolekcja działa w środku, ma 3 metody, Current-aktualny element, MoveNext, Reset
+        {
             var delta = (b * b) - (4 * a * c);
+            yield return  (-b + Math.Sqrt(delta)) / (2 * a);
+            yield return  (-b - Math.Sqrt(delta)) / (2 * a);
 
-            if (delta > 0)
-            {
-                MiejscePierwsze = (-b + Math.Sqrt(delta)) / (2 * a);  //w poprzedniej wersji było źle bo nie było nawiasów, czy test to wykaże?
-                MiejsceDrugie = (-b - Math.Sqrt(delta)) / (2 * a);
-
-                return new Wynik
-                {
-                    MiejscePierwsze = MiejscePierwsze,
-                    MiejsceDrugie = MiejsceDrugie
-                };
-            }
-            else if (delta < 0)
-            {
-                return(null); //"brak miejsc zerowych"
-            }
-            else // delta = 0
-            {
-                MiejscePierwsze = MiejsceDrugie = -b / (2 * a);
-
-                return new Wynik
-                {
-                    MiejscePierwsze = MiejscePierwsze,
-                    MiejsceDrugie = MiejsceDrugie
-                };
-            }
 
         }
     }
