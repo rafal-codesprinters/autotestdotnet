563b80eb9d80edde83e904f03029614d32c79f45
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2016-12-13 17:55:55 +0100

    after second dy

---
+++ selenium-automat/Automat/Selenium.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Text;
+using OpenQA.Selenium;
+using Xunit;
+using OpenQA.Selenium.Chrome;
+
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    public class Selenium : IDisposable
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        public Selenium()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            baseURL = "https://autotestdotnet.wordpress.com/";
+            verificationErrors = new StringBuilder();
+            driver.Manage()
+                .Timeouts()
+                .ImplicitlyWait(TimeSpan.FromSeconds(10));
+        }
+
+        protected void waitForElementClickable(By by, int seconds)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+            wait.Until(ExpectedConditions.ElementToBeClickable(by));
+        }
+        
+        [Fact]
+        public void TheSeleniumTest()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/wp-login.php?redirect_to=https%3A%2F%2Fautotestdotnet.wordpress.com%2Fwp-admin%2F&reauth=1");
+            driver.FindElement(By.Id("user_login")).Clear();
+            driver.FindElement(By.Id("user_login")).SendKeys("autotestdotnet@gmail.com");
+            driver.FindElement(By.Id("user_pass")).Clear();
+            driver.FindElement(By.Id("user_pass")).SendKeys("codesprinters2016");
+            driver.FindElement(By.Id("wp-submit")).Click();
+            driver.FindElement(By.LinkText("Posts")).Click();
+            driver.FindElement(By.LinkText("Add New")).Click();
+            driver.FindElement(By.Id("title")).Clear();
+            driver.FindElement(By.Id("title")).SendKeys("hello world");
+            driver.FindElement(By.Id("content")).Clear();
+            driver.FindElement(By.Id("content")).SendKeys("to jest jakaś długa testowa notatka");
+            driver.FindElement(By.Id("publish")).Click();
+            driver.Navigate().GoToUrl(baseURL + "/wp-login.php?action=logout");
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.Equal("", verificationErrors.ToString());
+        }
+    }
+}
