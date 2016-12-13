30dd74bb1c65ad20de858601cd2c959e846448be
Damian Bednarczyk <damian.bednarczyk@kotrak.pl>
2016-12-13 10:25:19 +0100

    Dodanie dwóch przypadków

--- FunkcjaKwadratowa/ClassLibrary1/ClassLibrary1/FunkcjaKwadratowa.cs
+++ FunkcjaKwadratowa/ClassLibrary1/ClassLibrary1/FunkcjaKwadratowa.cs
@@ -11,29 +11,55 @@ namespace ClassLibrary1
     {
 
         [Fact]
-        public void Wartosc_Ujemna()
+        public void Weryfikacja_ze_potrafimy_znalezc_dwa_miejsca_zerowe()
         {
             var a = 1;
             var b = -4;
             var c = 3;
 
-            var wynik = Oblicz(a, b, c);
+            var wynik = Oblicz(a, b, c).ToList();
             
-            Assert.Equal(3, wynik.MiejscePierwsze);
-            Assert.Equal(1, wynik.MiejsceDrugie);
+            Assert.Equal(3, wynik[0]);
+            Assert.Equal(1, wynik[1]);
             
         }
 
-        private wynik Oblicz(int a, int b, int c)
+        [Fact]
+        public void Weryfikacja_ze_potrafimy_znalezc_jedno_miejsce_zerowe()
         {
-            double x1 = 0;
-            double x2 = 0;
+            var a = 9;
+            var b = -12;
+            var c = 4;
+
+            var wynik = Oblicz(a, b, c);
+            double expected = (double)2 / 3;
+            //double expected = 2.0 / 3; //mozna tez tak
+            Assert.Equal(expected, wynik.First());
+
+        }
+
+        [Fact]
+        public void Weryfikacja_ze_funkcja_nie_zwraca_miejsc_zerowych()
+        {
+            var a = -6;
+            var b = 3;
+            var c = -1;
+
+            var wynik = Oblicz(a, b, c);
+            //Assert.NotEmpty(wynik);
+            var list = new List<int>();
+
+            Assert.Collection(wynik, item => Assert.Null(wynik));
+            //Assert.Collection(item => Assert.True(false), wynik);
+
+        }
+
+        private IEnumerable<double> Oblicz(double a, double b, double c)
+        {
 
             // todo napisz obliczanie rozwiązań (miejsc zerowych) funkcji kwadratowej 
             // jeśli nie pamiętasz jak to się liczy to tutaj jest ściąga
             // http://matma.prv.pl/kwadratowa.php
-            // postaraj się napisac to samodzielnie a nie googlując implementację
-            // powodzenia :)
             double delta = 0;
             double p = 0;
             double q = 0;
@@ -42,26 +68,9 @@ namespace ClassLibrary1
             p = -b / (2 * a);
             q = -delta / (4 * a);
 
-            //if (delta > 0)
-            //{
-            //    MiejscePierwsze = (-b + Math.Sqrt(delta)) / (2 * a);
-            //    MiejsceDrugie = (-b - Math.Sqrt(delta)) / (2 * a);
+            yield return (-b + Math.Sqrt(delta)) / (2 * a);
+            yield return (-b - Math.Sqrt(delta)) / (2 * a);
 
-            //}
-            //else
-            //{
-            //    MiejscePierwsze = -b / (2 * a);
-            //    MiejsceDrugie = null;
-
-            //}
-
-            x1 = (-b + Math.Sqrt(delta)) / (2 * a);
-            x2 = (-b - Math.Sqrt(delta)) / (2 * a);
-
-
-            wynik Wynik = new wynik {MiejscePierwsze = x1, MiejsceDrugie = x2 };
-
-            return Wynik;
         }
     }
 }
