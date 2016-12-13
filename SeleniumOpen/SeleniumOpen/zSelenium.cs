b997c4903ec7893786a3fbf98e874f7427e21727
Damian Bednarczyk <damian.bednarczyk@kotrak.pl>
2016-12-13 16:48:48 +0100

    TC Usuwanie notatki

--- SeleniumOpen/SeleniumOpen/zSelenium.cs
+++ SeleniumOpen/SeleniumOpen/zSelenium.cs
@@ -23,16 +23,16 @@ namespace SeleniumTests
             driver = new ChromeDriver();
             baseURL = "https://autotestdotnet.wordpress.com/";
             verificationErrors = new StringBuilder();
-            //driver.Manage().Window.Maximize();
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            driver.Manage().Window.Maximize();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
             driver.Manage()
                 .Timeouts()
-                .ImplicitlyWait(TimeSpan.FromSeconds(10));
+                .ImplicitlyWait(TimeSpan.FromSeconds(5));
         }
     
         
         [Fact]
-        public void TheZSeleniumTest()
+        public void Dodawanie_notatki()
         {
             driver.Navigate().GoToUrl(baseURL + "/wp-login.php?redirect_to=https%3A%2F%2Fautotestdotnet.wordpress.com%2Fwp-admin%2F&reauth=1");
             driver.FindElement(By.Id("user_login")).Clear();
@@ -41,9 +41,9 @@ namespace SeleniumTests
             driver.FindElement(By.Id("user_pass")).SendKeys("codesprinters2016");
             driver.FindElement(By.Id("user_pass")).Click();
             driver.FindElement(By.Id("wp-submit")).Click();
-            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='menu-posts']/a/div[3]")));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id='menu-posts']/a/div[3]")));
             driver.FindElement(By.XPath("//*[@id='menu-posts']/a/div[3]")).Click();
-            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//li[@id='menu-posts']/a/div[3]")));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//li[@id='menu-posts']/a/div[3]")));
             driver.FindElement(By.XPath("//li[@id='menu-posts']/a/div[3]")).Click();
             driver.FindElement(By.CssSelector("a.page-title-action")).Click();
             driver.FindElement(By.Id("title")).Clear();
@@ -55,6 +55,51 @@ namespace SeleniumTests
             driver.FindElement(By.Id("publish")).Click();
             driver.FindElement(By.LinkText("View post")).Click();
         }
+
+        [Fact]
+        public void Usuwanie_notatki()
+        {
+            var guid = Guid.NewGuid().ToString();
+
+            driver.Navigate().GoToUrl(baseURL + "/wp-login.php?redirect_to=https%3A%2F%2Fautotestdotnet.wordpress.com%2Fwp-admin%2F&reauth=1");
+            driver.FindElement(By.Id("user_login")).Clear();
+            driver.FindElement(By.Id("user_login")).SendKeys("autotestdotnet");
+            driver.FindElement(By.Id("user_pass")).Clear();
+            driver.FindElement(By.Id("user_pass")).SendKeys("codesprinters2016");
+            driver.FindElement(By.Id("user_pass")).Click();
+            driver.FindElement(By.Id("wp-submit")).Click();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id='menu-posts']/a/div[3]")));
+            driver.FindElement(By.XPath("//*[@id='menu-posts']/a/div[3]")).Click();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//li[@id='menu-posts']/a/div[3]")));
+            driver.FindElement(By.XPath("//li[@id='menu-posts']/a/div[3]")).Click();
+            driver.FindElement(By.CssSelector("a.page-title-action")).Click();
+            driver.FindElement(By.Id("title")).Clear();
+            driver.FindElement(By.Id("title")).SendKeys(guid);
+            driver.FindElement(By.Id("content-tmce")).Click();
+            driver.FindElement(By.Id("content-html")).Click();
+            driver.FindElement(By.Id("content")).Clear();
+            driver.FindElement(By.Id("content")).SendKeys(guid);
+            driver.FindElement(By.Id("publish")).Click();
+
+
+
+            //driver.Navigate().GoToUrl(baseURL + "/wp-login.php?redirect_to=https%3A%2F%2Fautotestdotnet.wordpress.com%2Fwp-admin%2F&reauth=1");
+            //driver.FindElement(By.Id("user_login")).Clear();
+            //driver.FindElement(By.Id("user_login")).SendKeys("autotestdotnet");
+            //driver.FindElement(By.Id("user_pass")).Clear();
+            //driver.FindElement(By.Id("user_pass")).SendKeys("codesprinters2016");
+            //driver.FindElement(By.Id("user_pass")).Click();
+            //driver.FindElement(By.Id("wp-submit")).Click();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id='menu-posts']/a/div[3]")));
+            driver.FindElement(By.XPath("//*[@id='menu-posts']/a/div[3]")).Click();
+            driver.FindElement(By.Id("post-search-input")).SendKeys(guid + Keys.Enter);
+            driver.FindElement(By.XPath("//tr/td[1]/strong/a")).Click();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id='delete-action']/a")));
+            driver.FindElement(By.XPath("//*[@id='delete-action']/a")).Click();
+            driver.FindElement(By.Id("post-search-input")).SendKeys(guid + Keys.Enter);
+            Assert.False(driver.FindElement(By.XPath("//tr/td[1]/strong/a")));
+        }
+
         private bool IsElementPresent(By by)
         {
             try
