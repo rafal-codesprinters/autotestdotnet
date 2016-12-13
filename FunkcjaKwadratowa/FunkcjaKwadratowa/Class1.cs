736443d63b252041405008b7d9ad3db256903361
Michał Kornacki <m.kornacki@benefitsystems.pl>
2016-12-13 10:44:34 +0100

    update

--- FunkcjaKwadratowa/FunkcjaKwadratowa/Class1.cs
+++ FunkcjaKwadratowa/FunkcjaKwadratowa/Class1.cs
@@ -8,33 +8,16 @@ using Xunit;
 namespace FunkcjaKwadratowa
 {
     public class FunkjaKwadratowa
-    { 
-        [Fact]
-        public void Weyfikacja_dwa_miejsca_zerowe()
-        {
-            double a = 1;
-            double b = -4;
-            double c = 3;
-            var wynik = oblicz(a, b, c).ToList();
-            Assert.Equal(1, wynik[0]);
-            Assert.Equal(3, wynik[1]);
-        }
-        [Fact]
-        public void Weyfikacja_jedno_miejsce_zerowe()
-        {
-            double a = 9;
-            double b = -12;
-            double c = 4;
-            var wynik = oblicz(a, b, c).ToList();
-            Assert.Equal((double)2 / 3, wynik[0]);
+    {
 
-        }
         [Theory]
-        [InlineData(9, -12, 5, default(double))]
-        public void NieMaMiejsc(double a, double b, double c, double first, double second)
+        [InlineData(9, -12, 5,new double[] {  })]
+        [InlineData(1, -4, 3, new double[] { 1, 3 })]
+        [InlineData(9, -12, 4, new double[] { (double)2 / 3 })]
+        public void NieMaMiejsc(double a, double b, double c,double[] result)
         {
             var wynik = oblicz(a, b, c).ToList();
-            Assert.Equal(default(double), wynik[0]);
+            Assert.Equal(result,wynik);
 
         }
 
@@ -42,13 +25,9 @@ namespace FunkcjaKwadratowa
         {
             var delta = Math.Pow(b, 2) - 4 * a * c;
 
-            if (delta < 0)
-            {
-                yield return default(double);
-            }
             if (delta == 0)
             {
-                yield return (double) (-1 * b) / (2 * a);
+                yield return (double)(-1 * b) / (2 * a);
             }
             if (delta > 0)
             {
@@ -56,6 +35,7 @@ namespace FunkcjaKwadratowa
                 yield return ((-1 * b) + Math.Sqrt(delta)) / 2 * a;
 
             }
+
         }
     }
 
