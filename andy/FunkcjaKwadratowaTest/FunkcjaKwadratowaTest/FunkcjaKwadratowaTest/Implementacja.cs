4f7622a4af1797d3d30b8bf520f46cced71aad9b
Andy Brandt <andy@andybrandt.net>
2016-12-13 10:11:39 +0100

    FunkcjaKwadratowaTest: refactored to use Func in Implementation

--- andy/FunkcjaKwadratowaTest/FunkcjaKwadratowaTest/FunkcjaKwadratowaTest/Implementacja.cs
+++ andy/FunkcjaKwadratowaTest/FunkcjaKwadratowaTest/FunkcjaKwadratowaTest/Implementacja.cs
@@ -8,23 +8,19 @@ namespace FunkcjaKwadratowaTest
 {
     class Implementacja
     {
-        
+
         public IEnumerable<double> Calculate(double a, double b, double c)
         {
-            double x1 = 0;
-            double x2 = 0;
-            double result = 0;
 
-            double delta = 0; // pośrednie obliczenie
-            delta = Math.Pow(b, 2) - (4 * a * c);
+            double delta = Math.Pow(b, 2) - (4 * a * c);
+            Func<double, double, double> x1 = (a1, b1) => (Math.Sqrt(delta) - b1) / (2 * a1);
 
             if (delta > 0)
             {
-                
-                x1 = (Math.Sqrt(delta) - b) / (2 * a);
-                x2 = (-Math.Sqrt(delta) - b) / (2 * a);
 
-                yield return x1;
+                double x2 = (-Math.Sqrt(delta) - b) / (2 * a);
+
+                yield return x1(a, b);
                 yield return x2;
 
             }
@@ -32,11 +28,9 @@ namespace FunkcjaKwadratowaTest
             {
                 if (delta == 0)
                 {
+                    yield return x1(a, b);
+                }
 
-                    x1 = (Math.Sqrt(delta) - b) / (2 * a);
-                    yield return x1;
-                } 
-                
             }
         }
     }
