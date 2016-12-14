664d6ea9f7d441fde0ac732f07ff8e5c91fb1afe
Damian Bednarczyk <damian.bednarczyk@kotrak.pl>
2016-12-14 09:39:25 +0100

    Dokonczenie usuwania notatki

--- SeleniumOpen/SeleniumOpen/zSelenium.cs
+++ SeleniumOpen/SeleniumOpen/zSelenium.cs
@@ -75,21 +75,10 @@ namespace SeleniumTests
             driver.FindElement(By.CssSelector("a.page-title-action")).Click();
             driver.FindElement(By.Id("title")).Clear();
             driver.FindElement(By.Id("title")).SendKeys(guid);
-            driver.FindElement(By.Id("content-tmce")).Click();
-            driver.FindElement(By.Id("content-html")).Click();
-            driver.FindElement(By.Id("content")).Clear();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("content")));
+            driver.FindElement(By.Id("content")).Click();
             driver.FindElement(By.Id("content")).SendKeys(guid);
             driver.FindElement(By.Id("publish")).Click();
-
-
-
-            //driver.Navigate().GoToUrl(baseURL + "/wp-login.php?redirect_to=https%3A%2F%2Fautotestdotnet.wordpress.com%2Fwp-admin%2F&reauth=1");
-            //driver.FindElement(By.Id("user_login")).Clear();
-            //driver.FindElement(By.Id("user_login")).SendKeys("autotestdotnet");
-            //driver.FindElement(By.Id("user_pass")).Clear();
-            //driver.FindElement(By.Id("user_pass")).SendKeys("codesprinters2016");
-            //driver.FindElement(By.Id("user_pass")).Click();
-            //driver.FindElement(By.Id("wp-submit")).Click();
             wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id='menu-posts']/a/div[3]")));
             driver.FindElement(By.XPath("//*[@id='menu-posts']/a/div[3]")).Click();
             driver.FindElement(By.Id("post-search-input")).SendKeys(guid + Keys.Enter);
@@ -97,7 +86,7 @@ namespace SeleniumTests
             wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id='delete-action']/a")));
             driver.FindElement(By.XPath("//*[@id='delete-action']/a")).Click();
             driver.FindElement(By.Id("post-search-input")).SendKeys(guid + Keys.Enter);
-            Assert.False(driver.FindElement(By.XPath("//tr/td[1]/strong/a")));
+            Assert.False(driver.FindElement(By.XPath("//tr/td[1]/strong/a")).Displayed);
         }
 
         private bool IsElementPresent(By by)
