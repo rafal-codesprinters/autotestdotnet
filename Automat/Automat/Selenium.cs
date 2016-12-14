ee7c49ff5f4b7457b483a4b19ced586e3a959713
unknown <piotrw@tema.com.pl>
2016-12-14 16:11:27 +0100

    Automat refactoring

--- Automat/Automat/Selenium.cs
+++ Automat/Automat/Selenium.cs
@@ -25,6 +25,9 @@ namespace SeleniumTests
 
             driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromMilliseconds(500));
+
+            WordPressConfig.Driver = driver;
+            WordPressConfig.BaseURL = baseURL;
         }
 
         public void Dispose()
@@ -146,6 +149,25 @@ namespace SeleniumTests
             Assert.True(driver.FindElements(By.XPath("//*[@id=\"content\"]/article")).Count > 0);
         }
 
+        [Fact]
+        public void Publish_New_Note()
+        {
+            LoginPage.Open();
+            LoginPage.User = ValidUser.Name;
+            LoginPage.Password = ValidUser.Password;
+            LoginPage.Login();
+
+            //AdminPage.Open();
+            AdminPage.OpenPosts();
+            AdminPage.OpenAddNewPost();
+            AdminPage.InsertPost("qwerty", "Ala ma kota");
+            AdminPage.Publish();
+
+            WordPress.Logout();
+        }
+
+        #region utils
+
         private bool IsElementPresent(By by)
         {
             try
@@ -202,6 +224,87 @@ namespace SeleniumTests
         {
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromMilliseconds(milisec));
             wait.Until(ExpectedConditions.ElementIsVisible(by));
+        }
+
+        #endregion
+
+        internal class WordPressConfig
+        {
+            public static string BaseURL { get; set; }
+            public static IWebDriver Driver { get; set; }
+        }
+
+        internal class ValidUser
+        {
+            public static string Name => "autotestdotnet@gmail.com";
+            public static string Password => "codesprinters2016";
+        }
+
+        internal class LoginPage
+        {
+            public static string Password { get; internal set; }
+            public static string User { get; internal set; }
+
+            internal static void Open()
+            {
+                WordPressConfig.Driver.Navigate().GoToUrl(WordPressConfig.BaseURL + "wp-admin/");
+            }
+
+            internal static void Login()
+            {
+                IWebDriver driver = WordPressConfig.Driver;
+                driver.FindElement(By.Id("user_login")).Click();
+                driver.FindElement(By.Id("user_login")).Clear();
+                driver.FindElement(By.Id("user_login")).SendKeys(User);
+                driver.FindElement(By.Id("user_pass")).Click();
+                driver.FindElement(By.Id("user_pass")).Clear();
+                driver.FindElement(By.Id("user_pass")).SendKeys(Password);
+                driver.FindElement(By.Id("rememberme")).Click();
+                driver.FindElement(By.Id("wp-submit")).Click();
+            }
+        }
+
+        internal class AdminPage
+        {
+            internal static void Open()
+            {
+            }
+
+            internal static void OpenPosts()
+            {
+                WordPressConfig.Driver.FindElement(By.XPath("//li[@id='menu-posts']/a/div[3]")).Click();
+            }
+
+            internal static void OpenAddNewPost()
+            {
+                WordPressConfig.Driver.FindElement(By.LinkText("Add New")).Click();
+            }
+
+            internal static void InsertPost(string caption, string content)
+            {
+                IWebDriver driver = WordPressConfig.Driver;
+                driver.FindElement(By.Id("title-prompt-text")).Click();
+                driver.FindElement(By.Id("title")).Clear();
+                driver.FindElement(By.Id("title")).SendKeys(caption);
+                driver.FindElement(By.Id("content")).Clear();
+                driver.FindElement(By.Id("content")).SendKeys(content);
+                driver.FindElement(By.Id("publish")).Click();
+                driver.FindElement(By.LinkText("My Site")).Click();
+            }
+
+            internal static void Publish()
+            {
+                WordPressConfig.Driver.FindElement(By.Id("publish")).Click();
+            }
+        }
+
+        internal class WordPress
+        {
+            internal static void Logout()
+            {
+                WordPressConfig.Driver.FindElement(By.CssSelector("img.avatar.avatar-32")).Click();
+                WordPressConfig.Driver.FindElement(By.CssSelector("button.ab-sign-out")).Click();
+            }
         }
     }
 }
