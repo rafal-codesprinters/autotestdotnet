d1722496f3e0eeaa437dd129f656f0aea2a80843
Damian Bednarczyk <damian.bednarczyk@kotrak.pl>
2016-12-14 12:40:37 +0100

    Poprawa pliku bat

--- autotest.bat
+++ autotest.bat
@@ -1,1 +1,1 @@
-autotestdotnet\SeleniumOpen\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe autotestdotnet\SeleniumOpen\SeleniumOpen\bin\Debug\SeleniumOpen.dll
+"autotestdotnet\SeleniumOpen\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe" "autotestdotnet\SeleniumOpen\SeleniumOpen\bin\Debug\SeleniumOpen.dll"
