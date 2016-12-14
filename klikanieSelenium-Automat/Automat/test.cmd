77a1ea8b4f64e34fce48f0dcbfde248a979500f0
Kris <klubartowski@gmail.com>
2016-12-14 12:24:57 +0100

    zmiana sciezki w cmd z bezwzglednej na wzgledna test.cmd

--- klikanieSelenium-Automat/Automat/test.cmd
+++ klikanieSelenium-Automat/Automat/test.cmd
@@ -1,1 +1,1 @@
-C:\autotestdotnet\klikanieSelenium-Automat\Automat\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe C:\autotestdotnet\klikanieSelenium-Automat\Automat\Automat\bin\Debug\Automat.dll
+packages\xunit.runner.console.2.1.0\tools\xunit.console.exe Automat\bin\Debug\Automat.dll
