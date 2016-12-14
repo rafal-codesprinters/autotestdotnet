f5dde607068449fe8538fb8bb5d6aa10f156d91f
Administrator <piotr.malecki@tema.com.pl>
2016-12-14 12:02:50 +0100

    testy selenium v6

--- test.cmd
+++ test.cmd
@@ -1,3 +1,2 @@
-cd\autotestdotnet\Selenium_zakladanie_posta\packages\xunit.runner.console.2.1.0\tools\
-rem xunit.console.exe c:\autotestdotnet\Selenium_zakladanie_posta\Selenium_zakladanie_posta\bin\Debug\Selenium_zakladanie_posta.dll
-cd\autotestdotnet\
+c:\autotestdotnet\Selenium_zakladanie_posta\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe c:\autotestdotnet\Selenium_zakladanie_posta\Selenium_zakladanie_posta\bin\Debug\Selenium_zakladanie_posta.dll
+
