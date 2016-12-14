66b2553da5e755d3f5396314f415d4872e0b7643
Arkadiusz Benedykt <abenedykt@rekord.com.pl>
2016-12-14 15:36:48 +0100

    refactoring to page objects

--- 4. selenium-automat/Selenium.cs
+++ 4. selenium-automat/Selenium.cs
@@ -3,67 +3,128 @@ using System.Text;
 using OpenQA.Selenium;
 using Xunit;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Interactions;
-using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumTests
 {
-    public class Selenium : IDisposable
+    public class Selenium
     {
-        private IWebDriver driver;
-        private StringBuilder verificationErrors;
-        private string baseURL;
-        private bool acceptNextAlert = true;
-
-        public Selenium()
-        {
-            driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            baseURL = "https://autotestdotnet.wordpress.com/";
-            verificationErrors = new StringBuilder();
-            driver.Manage()
-                .Timeouts()
-                .ImplicitlyWait(TimeSpan.FromSeconds(10));
-        }
-
-        protected void waitForElementClickable(By by, int seconds)
-        {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
-            wait.Until(ExpectedConditions.ElementToBeClickable(by));
-        }
-        
         [Fact]
-        public void TheSeleniumTest()
+        public void Moge_opublikowac_notatke()
         {
-            driver.Navigate().GoToUrl(baseURL + "/wp-login.php?redirect_to=https%3A%2F%2Fautotestdotnet.wordpress.com%2Fwp-admin%2F&reauth=1");
-            driver.FindElement(By.Id("user_login")).Clear();
-            driver.FindElement(By.Id("user_login")).SendKeys("autotestdotnet@gmail.com");
-            driver.FindElement(By.Id("user_pass")).Clear();
-            driver.FindElement(By.Id("user_pass")).SendKeys("codesprinters2016");
-            driver.FindElement(By.Id("wp-submit")).Click();
-            driver.FindElement(By.LinkText("Posts")).Click();
-            driver.FindElement(By.LinkText("Add New")).Click();
-            driver.FindElement(By.Id("title")).Clear();
-            driver.FindElement(By.Id("title")).SendKeys("hello world");
-            driver.FindElement(By.Id("content")).Clear();
-            driver.FindElement(By.Id("content")).SendKeys("to jest jakaś długa testowa notatka");
-            driver.FindElement(By.Id("publish")).Click();
-            driver.Navigate().GoToUrl(baseURL + "/wp-login.php?action=logout");
+            Test.Start();
+            StronaLogowania.Otworz();
+            StronaLogowania.Uzytkownik(PoprawnyUzytkownik.Nazwa);
+            StronaLogowania.Haslo(PoprawnyUzytkownik.Haslo);
+            StronaLogowania.Zaloguj();
 
-            
+            StronaAdministracyjna.Otworz();
+            StronaAdministracyjna.OtworzDodanieNowegoPosta();
+            StronaAdministracyjna.Wpis("jakis temat", "jakaś treść");
+            StronaAdministracyjna.Opublikuj();
+
+            WordPress.Wyloguj();
+            Test.Koniec();
         }
+    }
 
-        public void Dispose()
+    internal class Test
+    {
+        internal static IWebDriver Driver;
+        private static object verificationErrors;
+
+        internal static void Koniec()
         {
             try
             {
-                driver.Quit();
+                Driver.Quit();
             }
             catch (Exception)
             {
                 // Ignore errors if unable to close the browser
             }
             Assert.Equal("", verificationErrors.ToString());
+        }
+
+        internal static void Start()
+        {
+            Driver = new ChromeDriver();
+            Driver.Manage().Window.Maximize();
+            verificationErrors = new StringBuilder();
+            Driver.Manage()
+                .Timeouts()
+                .ImplicitlyWait(TimeSpan.FromSeconds(10));
+        }
+    }
+
+    internal class PoprawnyUzytkownik
+    {
+        public static string Haslo => "codesprinters2016";
+        public static string Nazwa => "autotestdotnet@gmail.com";
+    }
+
+    internal static class WordPress
+    {
+        internal static void Wyloguj()
+        {
+            Test.Driver.Navigate().GoToUrl(WordpressConfiguration.BaseUrl + "/wp-login.php?action=logout");
+        }
+    }
+
+    internal class WordpressConfiguration
+    {
+        public static string BaseUrl => "https://autotestdotnet.wordpress.com/";
+    }
+
+    internal static class StronaLogowania
+    {
+        internal static void Haslo(string haslo)
+        {
+            var user_element = By.Id("user_pass");
+            Test.Driver.FindElement(user_element).Clear();
+            Test.Driver.FindElement(user_element).SendKeys(haslo);
+        }
+
+        internal static void Otworz()
+        {
+            Test.Driver.Navigate().GoToUrl(WordpressConfiguration.BaseUrl + "/wp-login.php?redirect_to=https%3A%2F%2Fautotestdotnet.wordpress.com%2Fwp-admin%2F&reauth=1");
+        }
+
+        internal static void Uzytkownik(string uzytkownik)
+        {
+            Test.Driver.FindElement(By.Id("user_login")).Clear();
+            Test.Driver.FindElement(By.Id("user_login")).SendKeys(uzytkownik);
+        }
+
+        internal static void Zaloguj()
+        {
+            Test.Driver.FindElement(By.Id("wp-submit")).Click();
+        }
+    }
+
+    internal static class StronaAdministracyjna
+    {
+        internal static void Opublikuj()
+        {
+            Test.Driver.FindElement(By.Id("publish")).Click();
+        }
+
+        internal static void Otworz()
+        {
+            Test.Driver.FindElement(By.LinkText("Posts")).Click();
+        }
+
+        internal static void OtworzDodanieNowegoPosta()
+        {
+            Test.Driver.FindElement(By.LinkText("Add New")).Click();
+        }
+
+
+        internal static void Wpis(string temat, string tresc)
+        {
+            Test.Driver.FindElement(By.Id("title")).Clear();
+            Test.Driver.FindElement(By.Id("title")).SendKeys(temat);
+            Test.Driver.FindElement(By.Id("content")).Clear();
+            Test.Driver.FindElement(By.Id("content")).SendKeys(tresc);
         }
     }
 }
