cfa4565a4b4c7f7d2c4c966ddc42c50980affac9
Piotr Grzegorczyk <piotr.grzegorczyk@tema.com.pl>
2016-12-14 12:06:54 +0100

    Wyłączone zakłądanie postów przy stronicowanieu.

--- SeleniumCs/SeleniumCs/SeleniumCs/Test 03 - Zakładanie posta wersja 2 CS.cs
+++ SeleniumCs/SeleniumCs/SeleniumCs/Test 03 - Zakładanie posta wersja 2 CS.cs
@@ -97,23 +97,23 @@ namespace SeleniumTests
         [Fact]
         public void Stronicowanie()
         {
-            Login();
-            for (var i = 0; i < 6; i++)
-            {
-                driver.FindElement(By.XPath(@"//li[@id='menu-posts']/a")).Click();
-                driver.FindElement(By.LinkText("Add New")).Click();
-                new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementToBeClickable(By.Id("publish")));
-                driver.FindElement(By.Id("title")).Click();
-                driver.FindElement(By.Id("title")).Clear();
-                driver.FindElement(By.Id("title")).SendKeys("test pg " + i);
-                driver.FindElement(By.Id("content")).Click();
-                driver.FindElement(By.Id("content")).Clear();
-                driver.FindElement(By.Id("content")).SendKeys("test pg " + i);
-                new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementToBeClickable(By.Id("publish")));
-                driver.FindElement(By.Id("publish")).Click();
-                new WebDriverWait(driver, TimeSpan.FromSeconds(20)).Until(ExpectedConditions.ElementExists(By.CssSelector("#message")));
-            }
-            Logout();
+            //Login();
+            //for (var i = 0; i < 6; i++)
+            //{
+            //    driver.FindElement(By.XPath(@"//li[@id='menu-posts']/a")).Click();
+            //    driver.FindElement(By.LinkText("Add New")).Click();
+            //    new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementToBeClickable(By.Id("publish")));
+            //    driver.FindElement(By.Id("title")).Click();
+            //    driver.FindElement(By.Id("title")).Clear();
+            //    driver.FindElement(By.Id("title")).SendKeys("test pg " + i);
+            //    driver.FindElement(By.Id("content")).Click();
+            //    driver.FindElement(By.Id("content")).Clear();
+            //    driver.FindElement(By.Id("content")).SendKeys("test pg " + i);
+            //    new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementToBeClickable(By.Id("publish")));
+            //    driver.FindElement(By.Id("publish")).Click();
+            //    new WebDriverWait(driver, TimeSpan.FromSeconds(20)).Until(ExpectedConditions.ElementExists(By.CssSelector("#message")));
+            //}
+            //Logout();
             driver.Navigate().GoToUrl(baseURL);
             var beforeUrl = driver.Url;
             driver.FindElement(By.XPath(@"//*[@id=""nav-below""]/div[@class=""nav-previous""]/a")).Click();
