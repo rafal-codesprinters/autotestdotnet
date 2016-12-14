bd72dc2e3e96a7b05954f6b253b7334658e93653
Kris <klubartowski@gmail.com>
2016-12-14 12:37:46 +0100

    zmiana sciezki w teamcity na gorna galez

--- test.cmd
+++ test.cmd
@@ -1,1 +1,1 @@
-klikanieSelenium-Automat\Automat\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe klikanieSelenium-Automat\Automat\Automat\bin\Debug\Automat.dll
+"klikanieSelenium-Automat\Automat\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe" "klikanieSelenium-Automat\Automat\Automat\bin\Debug\Automat.dll"
