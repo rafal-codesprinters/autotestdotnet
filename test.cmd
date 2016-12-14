33981199300f057c0c9c0365d0bfd0a028ea1994
Izabela Lomotowska <ilo@scalepoint.com>
2016-12-14 12:46:51 +0100

    zmiana lokalizacji pliku

---
+++ test.cmd
@@ -0,0 +1,1 @@
+"2016-12-12\Przedszkoleniowe zadania\Iza\Selenium-Automat\Automat\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe" "2016-12-12\Przedszkoleniowe zadania\Iza\Selenium-Automat\Automat\Automat\bin\Debug\Automat.dll"
