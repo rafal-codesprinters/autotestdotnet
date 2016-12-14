3e5a9a2842908712ed2049197875bdd254c85b56
unknown <piotrw@tema.com.pl>
2016-12-14 12:30:05 +0100

    Automat run_test

--- Automat/run_test.cmd
+++ Automat/run_test.cmd
@@ -1,2 +1,2 @@
-c:\autotestdotnet\Automat\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe c:\autotestdotnet\Automat\Automat\bin\Debug\Automat.dll
+packages\xunit.runner.console.2.1.0\tools\xunit.console.exe Automat\bin\Release\Automat.dll
 
---
+++ Automat/run_test_02.cmd
@@ -0,0 +1,2 @@
+c:\autotestdotnet\Automat\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe c:\autotestdotnet\Automat\Automat\bin\Debug\Automat.dll
+
