6d3dc9d2da34c24a3e16063eaeb95dd36e0e08d3
Administrator <piotr.malecki@tema.com.pl>
2016-12-14 12:44:00 +0100

    team City v2

--- test.cmd
+++ test.cmd
@@ -1,2 +1,2 @@
-Selenium_zakladanie_posta\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe Selenium_zakladanie_posta\Selenium_zakladanie_posta\bin\Relase\Selenium_zakladanie_posta.dll
+Selenium_zakladanie_posta\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe Selenium_zakladanie_posta\Selenium_zakladanie_posta\bin\Release\Selenium_zakladanie_posta.dll
 
