7a0e67e1d0a0046e67299a954802ffb836e23fbd
Szymon Szewczyk <mjnuji@wp.pl>
2016-12-14 10:37:44 +0100

    cztery test casey dodane, zainstalowana paczka xunit runner i geckodriver y Please enter the commit message for your changes. Lines starting

--- IDE_WordPress/IDE_WordPress/exported_test_case_from_IDE.cs
+++ IDE_WordPress/IDE_WordPress/exported_test_case_from_IDE.cs
@@ -3,7 +3,9 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 using Xunit;
 
 namespace SeleniumTests
@@ -14,33 +16,113 @@ namespace SeleniumTests
         private StringBuilder verificationErrors;
         private string baseURL;
         private bool acceptNextAlert = true;
+        private string tematNotatki;
 
         public ExportedTestCaseFromIDE()
         {
-            driver = new FirefoxDriver();
+            driver = new ChromeDriver();
             baseURL = "https://autotestdotnet.wordpress.com/";
             verificationErrors = new StringBuilder();
+            driver.Manage().Window.Maximize();
+            tematNotatki = "Notatka ID:" + getGuid();
         }
+
         
-        
+
         [Fact]
-        public void TheExportedTestCaseFromIDETest()
+        public void Logging()
         {
-            driver.Navigate().GoToUrl("https://autotestdotnet.wordpress.com/wp-admin/");
-            Assert.Equal("Site Title ‹ Log In", driver.Title);
-            driver.FindElement(By.Id("user_login")).Clear();
-            driver.FindElement(By.Id("user_login")).SendKeys("autotestdotnet@gmail.com");
-            driver.FindElement(By.Id("user_pass")).Clear();
-            driver.FindElement(By.Id("user_pass")).SendKeys("codesprinters2016");
-            driver.FindElement(By.Id("wp-submit")).Click();
-            driver.FindElement(By.LinkText("Add New")).Click();
+            MetodaLogowania();
+            Assert.Equal("My Site", driver.FindElement(By.LinkText("My Site")).Text);
+            MetodaWylogowania();
+        }
+
+        [Fact]
+        public void Logowanie_TworzenieNotatki_Wylogowanie_i_Sprawdzenie_Czy_Widoczna ()
+        {
+            MetodaLogowania();
+            MetodaTworzenieNotatki();
+
+            String postLink = driver.FindElement(By.CssSelector("#message>p>a")).GetAttribute("href"); //znalazienie i zapisanie linku do stworzonego posta
+
+            MetodaWylogowania();
+            driver.Navigate().GoToUrl(postLink);
+            Assert.Equal(tematNotatki, driver.FindElement(By.CssSelector(".post-title>h1")).Text);
+        }
+
+        [Fact]
+        public void TworzenieIUsuniecieNotatki ()
+        {
+            MetodaLogowania();
+            MetodaTworzenieNotatki();
+        driver.Navigate().GoToUrl("https://wordpress.com/posts/autotestdotnet.wordpress.com");
+            waitForElementClickable(By.CssSelector("div.search__icon-navigation"), 10);
+
+            driver.FindElement(By.CssSelector("div.search__icon-navigation")).Click();
+            //driver.FindElement(By.CssSelector("#search-component-5")).Click();
+        }
+
+        [Fact]
+        public void SprawdzenieCzyJestDrugaStrona()
+        {
+            driver.Navigate().GoToUrl("https://autotestdotnet.wordpress.com/");
+            Assert.Equal("← Older posts", driver.FindElement(By.LinkText("← Older posts")).Text);
+            String url1 = driver.Url;
+            driver.FindElement(By.CssSelector(".nav-previous>a")).Click();
+            String url2 = driver.Url;
+            IWait<IWebDriver> wait = new OpenQA.Selenium.Support.UI.WebDriverWait(driver, TimeSpan.FromSeconds(30.00));
+            wait.Until(driver1 => ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"));
+            Assert.NotEqual(url1, url2);
+        }
+
+
+
+
+
+
+
+        protected void waitForElementClickable(By by, int seconds)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+            wait.Until(ExpectedConditions.ElementToBeClickable(by));
+        }
+
+        protected void MetodaLogowania ()
+        {
+        driver.Navigate().GoToUrl("https://autotestdotnet.wordpress.com/wp-admin/");
+        Assert.Equal("Site Title ‹ Log In", driver.Title);
+        driver.FindElement(By.Id("user_login")).Clear();
+        driver.FindElement(By.Id("user_login")).SendKeys("autotestdotnet@gmail.com");
+        driver.FindElement(By.Id("user_pass")).Clear();
+        driver.FindElement(By.Id("user_pass")).SendKeys("codesprinters2016");
+        driver.FindElement(By.Id("wp-submit")).Click();
+        }
+
+        protected void MetodaTworzenieNotatki()
+        {
+            driver.Navigate().GoToUrl("https://autotestdotnet.wordpress.com/wp-admin/post-new.php");
             driver.FindElement(By.Id("title")).Clear();
-            driver.FindElement(By.Id("title")).SendKeys("Notatka 34");
+            driver.FindElement(By.Id("title")).SendKeys(tematNotatki);
             driver.FindElement(By.Id("content")).Clear();
-            driver.FindElement(By.Id("content")).SendKeys("tresc");
+            driver.FindElement(By.Id("content")).SendKeys("tresc 35");
             driver.FindElement(By.Id("publish")).Click();
+            waitForElementClickable(By.Id("post-status-display"), 10);
             Assert.Equal("Published", driver.FindElement(By.Id("post-status-display")).Text);
         }
+
+        protected void MetodaWylogowania()
+        {
+            driver.Navigate().GoToUrl("https://wordpress.com/me");
+            waitForElementClickable(By.CssSelector(".button.me-sidebar__signout-button.is-compact"), 10);
+            driver.FindElement(By.CssSelector(".button.me-sidebar__signout-button.is-compact")).Click();
+        }
+
+        protected string getGuid ()
+        {
+            var guid = Guid.NewGuid().ToString();
+            return guid;
+        }
+
         private bool IsElementPresent(By by)
         {
             try
